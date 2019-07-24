de3077ab9c42dba8c309da78ba5923eb09b089d6
Dave Syer <dsyer@pivotal.io>
2019-07-24 10:56:08 +0100

    Convert to Webflux

--- src/main/java/ninja/siden/uml/Uml.java
+++ src/main/java/ninja/siden/uml/Uml.java
@@ -3,7 +3,6 @@ package ninja.siden.uml;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Map;
 import java.util.Set;
 
 import net.sourceforge.plantuml.FileFormat;
@@ -13,19 +12,21 @@ import net.sourceforge.plantuml.code.Transcoder;
 import net.sourceforge.plantuml.code.TranscoderUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 
 /**
  * @author taichi
@@ -50,42 +51,49 @@ public class Uml {
 	}
 
 	@PostMapping("/")
-	String outgoing(@RequestParam Map<String, String> form) throws Exception {
-		LOG.debug("" + form);
+	Mono<String> outgoing(ServerWebExchange exchange) throws Exception {
+		return exchange.getFormData().map(form -> form.toSingleValueMap()).map(form -> {
+			LOG.debug("" + form);
 
-		if (!tokens.contains(form.get("token"))) {
-			return ignored;
-		}
+			if (!tokens.contains(form.get("token"))) {
+				return ignored;
+			}
 
-		String tw = form.getOrDefault("trigger_word", "");
-		String txt = form.getOrDefault("text", "");
-		if (!txt.isEmpty()) {
-			txt = unescape(txt);
-		}
-		if (txt.length() < tw.length()) {
-			return ignored;
-		}
+			String tw = form.getOrDefault("trigger_word", "");
+			String txt = form.getOrDefault("text", "");
+			if (!txt.isEmpty()) {
+				txt = unescape(txt);
+			}
+			if (txt.length() < tw.length()) {
+				return ignored;
+			}
 
-		String content = txt.substring(tw.length()).trim();
-		String end = "@enduml";
-		if (content.endsWith(end)) {
-			content = content.substring(0, content.length() - end.length());
-		}
-		if (5000 < content.length()) {
-			StringBuilder stb = new StringBuilder();
+			String content = txt.substring(tw.length()).trim();
+			String end = "@enduml";
+			if (content.endsWith(end)) {
+				content = content.substring(0, content.length() - end.length());
+			}
+			if (5000 < content.length()) {
+				StringBuilder stb = new StringBuilder();
+				stb.append("{\"text\":\"");
+				stb.append("very large content has come. i cant process huge content.");
+				stb.append("\"}");
+				return stb.toString();
+			}
+
+			StringBuilder stb = new StringBuilder(100);
 			stb.append("{\"text\":\"");
-			stb.append("very large content has come. i cant process huge content.");
+			stb.append(url);
+			stb.append('/');
+			try {
+				stb.append(transcoder().encode(content));
+			}
+			catch (IOException e) {
+				throw new IllegalStateException(e);
+			}
 			stb.append("\"}");
 			return stb.toString();
-		}
-
-		StringBuilder stb = new StringBuilder(100);
-		stb.append("{\"text\":\"");
-		stb.append(url);
-		stb.append('/');
-		stb.append(transcoder().encode(content));
-		stb.append("\"}");
-		return stb.toString();
+		});
 	}
 
 	String unescape(String txt) {
@@ -97,20 +105,25 @@ public class Uml {
 		return TranscoderUtil.getDefaultTranscoder();
 	}
 
-	@GetMapping("/{encoded}")
-	ResponseEntity<byte[]> imgs(@PathVariable String encoded) throws Exception {
-		try {
-			String decoded = transcoder().decode(encoded);
-			ByteArrayOutputStream os = new ByteArrayOutputStream();
-			new SourceStringReader(decoded).generateImage(os,
-					new FileFormatOption(FileFormat.PNG, false));
-			return ResponseEntity.ok().contentType(new MediaType("image", "png"))
-					.body(os.toByteArray());
-		}
-		catch (Exception ex) {
-			LOG.error(ex.getMessage(), ex);
-			return ResponseEntity.notFound().build();
-		}
+	@GetMapping(path = "/{encoded}", produces = "image/png")
+	Mono<byte[]> imgs(@PathVariable String encoded) throws Exception {
+		return Mono.fromSupplier(() -> {
+			try {
+				String decoded = transcoder().decode(encoded);
+				ByteArrayOutputStream os = new ByteArrayOutputStream();
+				new SourceStringReader(decoded).generateImage(os,
+						new FileFormatOption(FileFormat.PNG, false));
+				return os.toByteArray();
+			}
+			catch (Exception ex) {
+				LOG.error(ex.getMessage(), ex);
+				throw new ResourceNotFoundException();
+			}
+		}).subscribeOn(Schedulers.elastic());
+	}
+
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public static class ResourceNotFoundException extends RuntimeException {
 	}
 
 	public static void main(String[] args) throws Exception {
--- src/test/java/ninja/siden/uml/UmlTest.java
+++ src/test/java/ninja/siden/uml/UmlTest.java
@@ -1,31 +1,27 @@
 package ninja.siden.uml;
 
 import org.junit.jupiter.api.Test;
+import reactor.blockhound.BlockHound;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import static org.hamcrest.CoreMatchers.endsWith;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.startsWith;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author taichi
  */
-@WebMvcTest(properties = { "url=http://example.com", "token=xxxxxxxxxx" })
+@WebFluxTest(properties = { "url=http://example.com", "token=xxxxxxxxxx" })
 public class UmlTest {
 
 	@Autowired
-	MockMvc mock;
+	WebTestClient mock;
 
 	@Test
 	public void outgoing() throws Exception {
@@ -36,16 +32,35 @@ public class UmlTest {
 		map.set("text", "@startuml\n" + content + "\n@enduml");
 
 		String enc = Uml.transcoder().encode(content);
-		mock.perform(post("/").contentType(MediaType.APPLICATION_FORM_URLENCODED).params(map))
-				.andExpect(jsonPath("$.text", startsWith("http://example.com")))
-				.andExpect(jsonPath("$.text", endsWith(enc)));
+		mock.post().uri("/").contentType(MediaType.APPLICATION_FORM_URLENCODED).body(map)
+				.exchange().expectStatus().isOk().expectBody().jsonPath("$.text")
+				.value((String text) -> valid(text, enc));
+	}
+
+	void valid(String text, String enc) {
+		assertThat(text).startsWith("http://example.com");
+		assertThat(text).endsWith(enc);
 	}
 
 	@Test
 	public void imgs() throws Exception {
 		String content = "Bob->Alice : hello";
 		String encoded = Uml.transcoder().encode(content);
-		mock.perform(get("/{encoded}", encoded)).andExpect(status().isOk())
-				.andExpect(content().string(notNullValue()));
+		EntityExchangeResult<byte[]> result = mock.get().uri("/{encoded}", encoded)
+				.exchange().expectStatus().isOk().expectHeader().contentType("image/png")
+				.expectBody().returnResult();
+		assertThat(result.getResponseBody()).isNotNull();
+	}
+
+	@Test
+	public void missing() throws Exception {
+		mock.get().uri("/{encoded}", "foo").exchange().expectStatus().isNotFound();
+	}
+
+	public static void main(String[] args) {
+		BlockHound.install();
+		System.setProperty("url", "http://localhost:8080");
+		System.setProperty("token", "xxxxx");
+		SpringApplication.run(Uml.class, args);
 	}
 }
